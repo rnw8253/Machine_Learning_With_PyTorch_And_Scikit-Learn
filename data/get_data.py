13a33673067b2a36c3805e42d08760da01dcaf93
Ryan Weber <ryan.weber@colostate.edu>
2024-02-20 19:52:08 -0600

    Sentiment Analysis

--- data/get_data.py
+++ data/get_data.py
@@ -2,10 +2,18 @@
 import pandas as pd
 
 
-columns = ['Overall Qual', 'Overall Cond', 'Gr Liv Area','Central Air', 'Total Bsmt SF', 'SalePrice']
-
-df = pd.read_csv('http://jse.amstat.org/v19n3/decock/AmesHousing.txt', sep='\t',usecols=columns)
-
-df.head()
-
-df.to_csv('ames_housing.csv', index=False, encoding='utf-8')
+#columns = ['Overall Qual', 'Overall Cond', 'Gr Liv Area','Central Air', 'Total Bsmt SF', 'SalePrice']
+#
+#df = pd.read_csv('http://jse.amstat.org/v19n3/decock/AmesHousing.txt', sep='\t',usecols=columns)
+#
+#df.head()
+#
+#df.to_csv('ames_housing.csv', index=False, encoding='utf-8')
+from sklearn.datasets import fetch_openml
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True, parser='auto')
+X = X.values
+y = y.values.astype(int)
+print(X.shape)
+print(y.shape)
+print(X)
+print(y)
